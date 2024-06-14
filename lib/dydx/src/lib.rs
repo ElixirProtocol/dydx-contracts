230877147a7f371abc4ed31f04499acfdb245ea2
Lou-Kamades <lou_kamades@proton.me>
2024-06-13 22:01:01 -0500

    copy dydx crate

--- lib/dydx/src/lib.rs
+++ lib/dydx/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod msg;
+mod querier;
+mod query;
+mod route;
+// mod dydx_types;
+mod proto_structs;
+mod serializable_int;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub use msg::{DydxMsg, Transfer, Order, OrderSide, OrderTimeInForce, OrderConditionType, OrderId};
+pub use querier::DydxQuerier;
+pub use query::{
+    MarketPriceResponse, PerpetualClobDetailsResponse, SubaccountResponse, DydxQuery, DydxQueryWrapper};
+pub use proto_structs::{
+    AssetPosition, ClobPair, MarketPrice, Metadata, PerpetualClobMetadata, PerpetualPosition, SpotClobMetadata, Status, Subaccount, SubaccountId
+};
+pub use serializable_int::SerializableInt;
+pub use route::DydxRoute;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// This export is added to all contracts that import this package, signifying that they require
+// "dydx" support on the chain they run on.
+#[no_mangle]
+extern "C" fn requires_dydx() {}
